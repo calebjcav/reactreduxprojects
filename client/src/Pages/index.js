32f6184b46cee32e1e09db9a501f873670e59d21
Dad <chris.d.caviness@gmail.com>
2020-06-27 03:57:36 -0500

    Login page added

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,6 +1,9 @@
 import vp from './VideoPage'
+import lp from './LoginPage'
 
 export const VideoPage = vp
+export const LoginPage = lp
 export default ({pages: {
-    VideoPage
+    VideoPage,
+    LoginPage
 }})
